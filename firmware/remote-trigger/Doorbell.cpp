95a103c814ffc83c1c2a75f8e94dd0d7593242f0
rpidanny <abhishekmaharjan1993@gmail.com>
2018-09-27 15:46:07 +0200

    Implement doorbell class with ring functionality

--- firmware/remote-trigger/Doorbell.cpp
+++ firmware/remote-trigger/Doorbell.cpp
@@ -1,11 +1,26 @@
 #include "Doorbell.h"
 
-Doorbell::Doorbell(uint8_t pin) {
-  PinMode(pin, OUTPUT);
+Doorbell::Doorbell(uint8_t pin, uint16_t bit_length, uint16_t frame_spacing, bool *frame, uint8_t length) {
+  _pin = pin;
+  _frame = frame;
+  _bit_count = length;
+  _bit_length = bit_length;
+  _frame_spacing = frame_spacing;
+
+  PinMode(_pin, OUTPUT);
 }
 
 void Doorbell::ring(uint8_t times) {
   for (uint8_t i = 0; i < times; i++) {
     // Ring button
+    sendFrame();
+    delayMicroseconds(_frame_spacing);
+  }
+}
+
+void Doorbell::sendFrame() {
+  for (uint8_t i = 0; i < _bit_count; i++) {
+    digitalWrite(_pin, _frame[i]);
+    delayMicroseconds(_bit_length);
   }
 }
--- firmware/remote-trigger/Doorbell.h
+++ firmware/remote-trigger/Doorbell.h
@@ -3,8 +3,24 @@
 
 class Doorbell {
   public:
-    Doorbell(uint8_t pin);
+    /**
+     * @param pin The RF data pin
+     * @param bit_length Bit length in microseconds
+     * @param frame_spacing Time between two frames in microseconds
+     * @param *frame Pointer to array of boolean denoting a frame
+     * @param length Number of bits in a frame
+     * @return void
+     **/
+    Doorbell(uint8_t pin, uint16_t bit_length, uint16_t frame_spacing, bool *frame, uint8_t length);
     void ring(uint8_t times = 1);
+  private:
+    bool *_frame;
+    uint9_t _pin; // RF data pin
+    uint8_t _bit_count; // number of bits in a frame
+    uint16_t _bit_length; // bit length in microseconds
+    uint16_t _frame_spacing; // frame spacing in microseconds
+
+    void sendFrame();
 };
 
 #endif
