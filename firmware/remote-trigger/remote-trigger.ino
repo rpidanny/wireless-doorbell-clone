8f57414ec1db771f825fca8e2ad7e3ebbc6d0699
rpidanny <abhishekmaharjan1993@gmail.com>
2018-09-27 20:22:23 +0200

    Moved vars to define

--- firmware/remote-trigger/remote-trigger.ino
+++ firmware/remote-trigger/remote-trigger.ino
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 #include "Doorbell.h"
 
+#define pin 13
 #define short_pulse 475
 #define long_pulse 1435
-
+#define frame_interval 8667
 // 0: short, 1: long
 bool frame[35] = {
   0, 1, 0, 1, 0, 0, 1,
@@ -13,7 +14,7 @@ bool frame[35] = {
   1, 0, 1, 0, 1, 0, 1
 };
 
-Doorbell d1(13, short_pulse, long_pulse, 8667, frame, 35);
+Doorbell d1(pin, short_pulse, long_pulse, frame_interval, frame, 35);
 
 void setup() {
   // put your setup code here, to run once:
