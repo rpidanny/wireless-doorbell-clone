09db935bec387d9de1384d609bc8786fc54cdf77
rpidanny <abhishekmaharjan1993@gmail.com>
2018-09-30 13:06:29 +0200

    Add button action

--- firmware/micropython/remote-trigger/scripts/main.py
+++ firmware/micropython/remote-trigger/scripts/main.py
@@ -1,10 +1,25 @@
 import pycom
+from machine import Pin
 from lib.doorbell import Doorbell
 
 # disable heartbeat led
 # pycom.heartbeat(False)
 
+# Pin connected to the RF tx pin
 rf_pin = 'G10'
 
+# Pin connected to button
+btn_pin = 'G17'
+
+# Init Doorbell
 bell = Doorbell(rf_pin)
-bell.ring(40)
+
+# Button press handler
+def btn_press_handler(arg):
+    print('Button pressed, Ringing bell!!')
+    bell.ring(40)
+
+# Init button pin
+btn = Pin(btn_pin, mode=Pin.IN, pull=Pin.PULL_UP)
+# Setup button press callback
+btn.callback(Pin.IRQ_FALLING, btn_press_handler)
